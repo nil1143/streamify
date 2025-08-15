55075d91949034d34a33be93370706340d5f2f21
John Doe <johndoe@email.com>
2025-08-15 18:25:15 +0100

    feat(project): initialize frontend and backend with basic setup

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+
+import authRoutes from "./routes/auth.route.js";
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
