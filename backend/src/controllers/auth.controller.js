a4131fe12ed87d1c6ea84fdbb07d4d4e29e51bee
John Doe <johndoe@email.com>
2025-08-16 15:45:40 +0100

    feat(backend): finalize authentication routes and integrate Stream API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
-// import { upsertStreamUser } from "../lib/stream.js";
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
+
+// Signup controller
 export async function signup(req, res) {
   const { email, password, fullName } = req.body;
 
@@ -72,10 +74,50 @@ export async function signup(req, res) {
   }
 }
 
+// Login controller
 export async function login(req, res) {
-  res.send("Login Route");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email" });
+    }
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
+
+// Logout controller
 export function logout(req, res) {
-  res.send("Logout Route");
+  res.clearCookie("jwt")
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 }
